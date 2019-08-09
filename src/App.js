953aeca294b3c6a87b5b49ed17bf6417f21fe5af
aakashsr <aakashsri.dev@gmail.com>
2019-08-09 22:52:02 +0530

    Removed the local state of Counter component and merge it's state with the Application state of App component , passed score as a prop from the App component to Counter component , and finally converted the Counter component back to functional component

--- src/App.js
+++ src/App.js
@@ -8,27 +8,31 @@ class App extends Component {
     players: [
       {
         name: "Guil",
+        score: 0,
         id: 1
       },
       {
         name: "Treasure",
+        score: 0,
         id: 2
       },
       {
         name: "Ashley",
+        score: 0,
         id: 3
       },
       {
         name: "James",
+        score: 0,
         id: 4
       }
     ]
   };
 
   removePlayer = id => {
-     this.setState(currentState => ({
-       players: currentState.players.filter(c => c.id !== id)
-     }));
+    this.setState(currentState => ({
+      players: currentState.players.filter(c => c.id !== id)
+    }));
   };
 
   render() {
@@ -39,6 +43,7 @@ class App extends Component {
           <Player
             key={player.id.toString()}
             id={player.id}
+            score={player.score}
             name={player.name}
             removePlayer={this.removePlayer}
             // score={player.score}
--- src/Counter.js
+++ src/Counter.js
@@ -1,41 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Counter extends Component {
-  state = {
-    score: 0
-  };
+const Counter = props => {
+  //   increaseScore = () => {
+  //     this.setState(prevState => ({
+  //       score: prevState.score + 1
+  //     }));
+  //   };
 
-  increaseScore = () => {
-    this.setState(prevState => ({
-      score: prevState.score + 1
-    }));
-  };
+  //   decreaseScore = () => {
+  //     this.setState((prevState) => ({
+  //       score: prevState.score - 1
+  //     }));
+  //   };
 
-  decreaseScore = () => {
-    this.setState((prevState) => ({
-      score: prevState.score - 1
-    }));
-  };
-
-  render() {
-    return (
-      <div className="counter">
-        <button
-          onClick={this.decreaseScore}
-          className="counter-action decrement"
-        >
-          -
-        </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button
-          onClick={this.increaseScore}
-          className="counter-action increment"
-        >
-          +
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="counter">
+      <button
+        //   onClick={this.decreaseScore}
+        className="counter-action decrement"
+      >
+        -
+      </button>
+      <span className="counter-score">{props.score}</span>
+      <button
+        //   onClick={this.increaseScore}
+        className="counter-action increment"
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
 export default Counter;
