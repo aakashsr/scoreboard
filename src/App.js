a8ddfba0c080a3da8357080efd4947ea481c7f04
aakashsr <aakashsri.dev@gmail.com>
2019-08-09 20:24:11 +0530

    Added a state to our App component and moved our players array into it so that user can delete or add any player

--- src/App.js
+++ src/App.js
@@ -3,36 +3,43 @@ import "./App.css";
 import Header from "./Header";
 import Player from "./Player";
 
-const players = [
-  {
-    name: "Guil",
-    score: 50,
-    id: 1
-  },
-  {
-    name: "Treasure",
-    score: 85,
-    id: 2
-  },
-  {
-    name: "Ashley",
-    score: 95,
-    id: 3
-  },
-  {
-    name: "James",
-    score: 80,
-    id: 4
-  }
-];
-
 class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Guil",
+        score: 50,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 85,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 95,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 80,
+        id: 4
+      }
+    ]
+  };
+
+  removePlayer = contact => {
+     this.setState(currentState => ({
+       contacts: currentState.contacts.filter(c => c.id !== contact.id)
+     }));
+  };
 
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalplayers={players.length} />
-        {players.map(player => (
+        <Header title="Scoreboard" totalplayers={this.state.players.length} />
+        {this.state.players.map(player => (
           <Player
             key={player.id.toString()}
             name={player.name}
