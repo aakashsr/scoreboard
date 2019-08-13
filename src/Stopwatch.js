cbd67d6fd3aab2d310f2a911de82df7b3880f112
aakashsr <aakashsri.dev@gmail.com>
2019-08-13 18:55:34 +0530

    Added state for elapsedTime  , previousTime and isRunning and used componentDidMount method to update our timer every 100ms.Also collected the value of previousTime as soon as user clicks on start button

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 
 export default class Stopwatch extends Component {
   state = {
-    isRunning: false
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
   };
 
-  changeValue = () => {
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 100);
+  }
+
+  tick = () => {
+    console.log("ticking...");
+  };
+
+  handleStopwatch = () => {
     this.setState(prevState => ({
       isRunning: !prevState.isRunning
     }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
   };
 
   render() {
@@ -16,7 +29,9 @@ export default class Stopwatch extends Component {
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button onClick={this.changeValue}>{this.state.isRunning ?"STOP":"START"}</button>
+        <button onClick={this.handleStopwatch}>
+          {this.state.isRunning ? "STOP" : "START"}
+        </button>
         <button>Reset</button>
       </div>
     );
