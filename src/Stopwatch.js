07bbf2d9b36cc17f3bee2e8a89c7471cf2507d92
aakashsr <aakashsri.dev@gmail.com>
2019-08-13 17:43:03 +0530

    Added functionality to toggle button between Stop and Start on clicking over it

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 
 export default class Stopwatch extends Component {
+  state = {
+    isRunning: false
+  };
+
+  changeValue = () => {
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+  };
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={this.changeValue}>{this.state.isRunning ?"STOP":"START"}</button>
+        <button>Reset</button>
       </div>
     );
   }
